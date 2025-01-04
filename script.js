4ca00fcffa90d578b81758f6759ef0b199591ce4
Will <will@willallstetter.com>
2025-01-04 10:32:43 -0500

    sooo close! (color sidebar working)

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const projects = [
         id: 'desktopDump',
         title: 'Desktop Dump Archive',
         year: '2020 - ongoing',
-        description: 'Website, multimedia files, archive is updated regularly',
+        description: 'Website, multimedia files, archive is updated regularly<br/>Unique',
         links: [
             { text: 'Archive', url: 'https://mollysoda.hotglue.me/?Desktop%20Dump%20Archive.head' },
             { text: 'Screen Walk', url: 'https://thephotographersgallery.org.uk/whats-on/watch-screen-walk-molly-soda' }
@@ -14,7 +14,7 @@ const projects = [
         id: 'steadierFooting',
         title: 'Steadier Footing',
         year: '2013 - ongoing',
-        description: 'Videos, YouTube playlist, additional video added yearly',
+        description: 'Videos, YouTube playlist, additional video added yearly<br/>Unique (Individual videos edition of 3, 1 AP)<br/>2019 video “I want to be alone” in permanent collection of FRAC Méca',
         links: [
             { text: 'Playlist', url: 'https://www.youtube.com/watch?v=FlCy6RI7iYw&list=PLC1Ry_tD1kTXSR_FdUz6XqIx_C4eog4jR&index=1&t=0s&ab_channel=mollysoda' }
         ],
@@ -65,20 +65,68 @@ const projects = [
             { text: 'Blog post', url: 'https://mollysoda.substack.com/p/the-girl-in-the-haircuts-folder' }
         ],
         image: 'photos/Screenshot 2025-01-03 at 5.16.25 PM.png'
+    },
+    {
+        id: 'tomatoGirl',
+        title: 'Tomato Girl',
+        year: '2023',
+        description: 'Video, 1 min 56 sec, Documentation of TikTok live stream, Edition of 3, 1AP',
+        links: [
+            { text: 'Video', url: 'https://www.youtube.com/watch?v=dRQVLNafI18' },
+            { text: 'Blog post', url: 'https://mollysoda.substack.com/p/you-say-tomato-i-say-tomato-girl' }
+        ],
+        image: 'photos/Screenshot 2025-01-03 at 5.33.07 PM.png'
+    },
+    {
+        id: 'cupid',
+        title: 'cupid',
+        year: '2023',
+        description: 'Two-channel video, 2 min 52 sec<br/>Webcam video printed out, doctored frame by frame, rescanned<br/>Edition of 3, 1AP',
+        links: [
+            { text: 'Video', url: 'https://www.youtube.com/watch?v=bvxhF5Qj4uQ' },
+            { text: 'Blog post', url: 'https://mollysoda.substack.com/p/notes-on-cupid' }
+        ],
+        image: 'photos/Screenshot 2025-01-03 at 5.38.21 PM.png'
     }
 ];
 
 function generateProjects() {
+    const colors = ['#ff00b3', '#00b3ff', '#b300ff', '#ffb300', '#00ffb3', '#b3ff00', '#ff00b3', '#00b3ff', '#b300ff', '#ffb300', '#00ffb3', '#b3ff00'];
     const projectsContainer = document.getElementById('projects');
     const sidebar = document.querySelector('.sidebar');
+    let i = 0;
 
     projects.forEach(project => {
         // Create sidebar icon
-        const icon = document.createElement('img');
-        icon.src = project.image;
+        const icon = document.createElement('div');
+        icon.style.width = '50px'; // Set width for the icon
+        icon.style.height = '50px'; // Set height for the icon
+        icon.style.position = 'relative'; // Set position to relative for layering
         icon.id = `${project.id}ICON`;
         icon.addEventListener('click', () => scrollToElement(project.id));
+        icon.addEventListener('mouseover', () => {
+            icon.style.backgroundColor = 'transparent'; // Change to transparent on hover
+        });
+        icon.addEventListener('mouseout', () => {
+            icon.style.backgroundColor = project.color; // Use saved color
+        });
+
+        const img = document.createElement('img');
+        img.src = project.image;
+        img.style.width = '100%'; // Set width for the img div
+        img.style.height = '100%'; // Set height for the img div
+        img.style.objectFit = 'cover'; // Ensure the image covers the entire div
+        img.style.position = 'absolute'; // Position image absolutely within the icon
+        img.style.top = '0'; // Align image to the top
+        img.style.left = '0'; // Align image to the left
+        img.style.zIndex = '-1'; // Set z-index for the image
+
+        icon.style.backgroundColor = colors[i]; // Set solid color for the icon
+        project.color = colors[i]; // Use saved color
+        i = (i + 1) % colors.length; // Cycle through colors
+        icon.style.zIndex = '1'; // Set z-index for the icon background
         sidebar.appendChild(icon);
+        icon.appendChild(img);
 
         // Create project section
         const projectDiv = document.createElement('div');
